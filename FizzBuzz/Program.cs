0733409cd835ec434aabd886aaa30c7decfff121
Lisovenko.VN <Lisovenko.VN@ADM-PROGRAM-03>
2024-10-05 21:36:42 +1000

    Добавьте файлы проекта.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            while (i <= 100)
+            {
+                string OutputI = i.ToString();
+
+                if (i % 3 == 0 & i % 5 == 0)
+                {
+                    OutputI = "FizzBuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    OutputI = "Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    OutputI = "Buzz";
+                }
+                
+                Console.WriteLine(OutputI);
+
+                i++;
+            }
+        }
+    }
+}
